59cec2b45bf9a4522fdebb721ebff1aee6be5ac8
Yu Yan <miracleyanyu@gmail.com>
2020-03-17 11:59:05 -0700

    Add CourseTableComponent

---
+++ src/app/course-table/course-table.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {CourseServiceClient} from '../services/CourseServiceClient';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-course-table',
+  templateUrl: './course-table.component.html',
+  styleUrls: ['./course-table.component.css']
+})
+export class CourseTableComponent implements OnInit {
+  faFile = faFile;
+
+  constructor(private service: CourseServiceClient) { }
+
+  courses = [];
+
+  ngOnInit(): void {
+    this.service.findAllCourses()
+    .then(courses => this.courses = courses);
+  }
+
+}
